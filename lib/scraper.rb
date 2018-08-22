da8300c5bbff1c874390e1da46b53fa96286b0b8
Michael Roose <mjroose@gmail.com>
2018-08-22 19:21:23 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = Nokogiri::HTML(open('./fixtures/student-site/index.html'))
+    html = Nokogiri::HTML(open(index_url))
     profile_links = html.css('div.student-card a')
     profile_links.collect do |profile_link|
       {
@@ -16,7 +16,7 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-
+    
   end
 
 end
