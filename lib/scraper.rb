7f849d581e9ded3ed3589785e4ffe264ccaadabb
Michael Roose <mjroose@gmail.com>
2018-08-22 19:34:35 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,15 +18,15 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = Nokogiri::HTML(open(profile_url))
     {
-      :name => html.css('div.vitals-text-container h1.profile-name').text,
-      :location => html.css('div.vitals-text-container h2.profile-location').text,
+      # :name => html.css('div.vitals-text-container h1.profile-name').text,
+      # :location => html.css('div.vitals-text-container h2.profile-location').text,
       :twitter => html.css('div.social-icon-container a')[0].attributes['href'].value,
       :linkedin => html.css('div.social-icon-container a')[1].attributes['href'].value,
       :github => html.css('div.social-icon-container a')[2].attributes['href'].value,
       :blog => html.css('div.social-icon-container a')[3].attributes['href'].value,
       :profile_quote => html.css('div.vitals-text-container div.profile-quote').text,
-      :bio => html.css('div.description-holder p').text,
-      :profile_url => profile_url
+      :bio => html.css('div.description-holder p').text
+      # :profile_url => profile_url
     }
   end
 
